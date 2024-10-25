ce1c401b86b157772041698d708dc5d0a91400e5
Hemantsudarshan <hemanthsudarshan2002@gmail.com>
2024-10-25 23:58:11 +0530

    Ga reverted

--- app.py
+++ app.py
@@ -146,18 +146,4 @@ def main():
 if __name__ == "__main__":
     main()
 
-# Google Analytics Tracking Code
-GA_JS = """
-<!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-9663NCZLHD"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
 
-  gtag('config', 'G-9663NCZLHD');
-</script>
-"""
-
-# Embed the Google Analytics script in the app
-components.html(GA_JS)
